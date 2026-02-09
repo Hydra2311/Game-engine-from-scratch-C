ee1be5433ea3dfa2da1854f1de269e930a1d9e0d
Hydra2311 <thodoris231107@gmail.com>
2026-02-09 15:40:45 +0200

    Blackjack end

--- luck.c
+++ luck.c
@@ -1,6 +1,7 @@
 #include "engine.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #define TRUE 1
 #define FALSE 0
 
@@ -12,10 +13,11 @@ int main()
         {"Jack", 10}, {"Queen", 10}, {"King", 10}, {"Ace", 1} 
     };
     int acecount=0;
-    int resi,result;
-    int firstace = TRUE,chosen1 = FALSE,card,hpoints=0,ppoints=0,pulls=0;
+    int i;
+    int resi,result,flag = TRUE;
+    int firstace = TRUE,chosen1 = FALSE,chosen11 = FALSE,card,firstcard,hpoints=0,ppoints=0;
     char housecards[5][10],playercards[5][10];
-    char c;
+    char c,r;
     while (TRUE)
     {
         fprintf(stdout,"1.Dice Roll 2.Blackjack 3.Quit: ");
@@ -69,31 +71,133 @@ int main()
             }
             case 2:
             {
-                while (TRUE)
+                firstcard = drawcard(Blackjack,sizeof(Blackjack));
+                printf("Hidden\n\n");
+                if (card != 12)
+                    hpoints += Blackjack[firstcard].points;
+                else
+                    hpoints = ace(&firstace,hpoints,&chosen1);                
+                while (flag)
                 {
                     card = drawcard(Blackjack,sizeof(Blackjack));
                     printf("%s\n\n",Blackjack[card].card);
                     if (card != 12)
                         hpoints += Blackjack[card].points;
                     else
-                        hpoints += ace(&firstace,hpoints,&chosen1);
+                        hpoints = ace(&firstace,hpoints,&chosen1);
                     if ((hpoints >= 15) && (hpoints < 21))
                     {
                         printf("The House is done.\nPlayer's turn.\n");
-                        break;
+                        flag = FALSE;
                     }
                     else if (hpoints == 21)
                     {
                         printf("The House has BLACKJACK!!!\nThe only to survive is with a BLACKJACK of your own!\n");
-                        break;
+                        flag = FALSE;
                     }
                     else if (hpoints > 21)
                     {
                         printf("The House has over 21.\nThe player WINS!!!\n");
-                        break;
+                        flag = FALSE;
                     }
                 }
+                flag = TRUE;
+                firstace = TRUE;
+                chosen1 = FALSE;
+                if (hpoints > 21)
+                {
+                    hpoints = 0;
+                    break;
+                }
+                usleep(200000);
+                printf("Your starting hand\n");
+                for(i=0;i<2;i++)
+                {
+                    card = drawcard(Blackjack,sizeof(Blackjack));
+                    printf("%s\n",Blackjack[card].card);
+                    if (card != 12)
+                        ppoints += Blackjack[card].points;
+                    else
+                    {
+                        if (firstace)
+                        {
+                            printf("Choose 1 or 11: ");
+                            scanf("%d",&resi);
+                            while ((c = getchar()) != '\n');
+                            if (resi == 1)
+                            {
+                                ppoints += 1;
+                                chosen1 = TRUE;
+                            }
+                            else if (resi == 11)
+                            {
+                                ppoints += 11;
+                                chosen11 = TRUE;
+                            }
+                        }
+                        else
+                        {
+                            if (chosen11 == TRUE)
+                                ppoints += 1;
+                            else if (chosen1 == TRUE)
+                                ppoints += 11;
+                        }
+                    }
+                    usleep(100000);
+                }
+                while (flag)
+                {
+                    if (ppoints > 21)
+                    {
+                        printf("Over 21. You LOSE!!!\n");
+                        break;
+                    }
+                    else if (ppoints == 21)
+                    {
+                        printf("BLACKJACK!!!\n");
+                        if (hpoints == 21)
+                            printf("Sadly it's a TIE\n");
+                        else
+                            printf("You WIN!!!\n");
+                        break;
+                    }
+                    printf("Your total points are %d\n",ppoints);
+                    printf("Do you wish to draw again?[y/n] ");
+                    r = getchar();
+                    while ((c = getchar()) != '\n');
+                    switch (r)
+                    {
+                        case 'y': case 'Y': 
+                        {
+                            card = drawcard(Blackjack,sizeof(Blackjack));
+                            ppoints += Blackjack[card].points;
+                            printf("%s\n",Blackjack[card].card);  
+                            break;  
+                        }
+                        case 'n': case 'N':
+                        {
+                            printf("Revealing the Hidden : %s\n",Blackjack[firstcard].card);
+                            if (ppoints > hpoints)
+                                printf("You WIN!!!\n");
+                            else if (ppoints <= hpoints)
+                                printf("you LOSE!!!\n");
+                            flag = FALSE;
+                            break;
+                        }
+                        default:
+                        {
+                            printf("Not valid");
+                            break;
+                        }
+                    }
+                }
+                ppoints = 0;
                 hpoints = 0;
+                
+                firstace = TRUE;
+                chosen11 = FALSE;
+                chosen1 = FALSE;
+                flag = TRUE;
                 break;
             }
             case 3:
